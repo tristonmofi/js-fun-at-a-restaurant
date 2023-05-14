36b3126362f3c142ae47ac09b6b5801ed59c097f
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-05-14 12:02:57 -0600

    Order.js complete

--- src/order.js
+++ src/order.js
@@ -1,7 +1,39 @@
+function takeOrder(orders, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+  return deliveryOrders.push(orders)
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1) 
+    } 
+  } 
+}
+
+ 
+
+function listItems(deliveryOrders) {
+  newArray = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    newArray.push(deliveryOrders[i].item)
+    }
+    return newArray.join(', ')
+  }  
+
+  
+function searchOrder(deliveryOrders, food) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === food) {
+      return true 
+    }
+  } return false
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }

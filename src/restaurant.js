b6a8caa2b9b39535fd82b50ca41bbb8109e3143f
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-05-14 15:07:02 -0600

    1/2 of restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,33 @@
+function createRestaurant(name) {
+  return pizzaRestaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    },
+  } 
+}
+
+function addMenuItem(pizzaRestaurant, food) {
+    if (pizzaRestaurant.menus[food.type].includes(food) === false) {
+      return pizzaRestaurant.menus[food.type].push(food)
+    }
+  }
+
+
+
+
+
+
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+createRestaurant, 
+addMenuItem,
   // removeMenuItem,
   // checkForFood
 }
